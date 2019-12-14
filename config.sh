9d101a5bf387baf9938e791e43b26db779e952e5
Yiğit Kemal Erinç <yigit.erinc@ozu.edu.tr>
2019-12-14 23:34:22 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -9,6 +9,7 @@ tar -xf nginx-1.13.8.tar.gz
 cd nginx-1.13.8/
 sudo apt update
 sudo apt install autoconf automake build-essential libpcre3 libpcre3-dev libssl-dev
+sudo apt-get install zlib1g-dev
 ./configure --with-http_ssl_module --add-module=../nginx-rtmp-module --with-http_stub_status_module --add-module=../nginx-ts-module
 make
 sudo make install
